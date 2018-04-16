7684ac790e5cd78974d1f4236fd66d81b67a9fb9
Everton L Baroni <evertonlbaroni@gmail.com>
2018-04-16 19:05:35 -0300

    Inclusão de 'traduções' e estilo para view

---
+++ resources/lang/pt-br/views.php
@@ -0,0 +1,13 @@
+<?php
+
+return
+    [
+        'best' => 'Melhores',
+        'news' => 'Notiícia',
+        'recent' => 'Recente',
+        'top' => 'Top',
+        'comments' => 'Comentários',
+        'points' => 'Pontos',
+        'answer' => 'Responstas',
+        'access_news' => 'Acessar Notícia'
+    ];
