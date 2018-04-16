54584aceda392ccbae32e537d27ae82dc3950cd1
Everton L Baroni <evertonlbaroni@gmail.com>
2018-04-16 18:53:28 -0300

    Criação das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('news', 'NewsController')
+    ->only(['index', 'show']);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+   return redirect(route('news.index'));
 });
